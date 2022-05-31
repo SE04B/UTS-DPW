322e1bb8d326c8592cb98fbd7be7b8d77c33a896
Mirfani340 <muhammadirfani340@gmail.com>
2022-05-31 21:23:18 +0700

    dev update

---
+++ admin/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $host = '127.0.0.1';
+    $username = 'root';
+    $password = '';
+    $database = 'user';
+    $connection = mysqli_connect($host,$username,$password,$database);
+
+?>
