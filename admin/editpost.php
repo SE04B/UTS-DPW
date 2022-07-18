2ac3cb04ea97663765c2a76d4a6a426c6c9d0fd0
muhammadnazaralwi <20104038@ittelkom-pwt.ac.id>
2022-07-18 23:53:23 +0700

    Change to News instead of Dashboard

--- admin/editpost.php
+++ admin/editpost.php
@@ -102,8 +102,8 @@ if ( isset( $_POST['post-update'])) {
    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
       <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
-          <a href="Dashboard.php" class="list-group-item list-group-item-action py-2 ripple active" aria-current="true">
-            <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
+          <a href="News.php" class="list-group-item list-group-item-action py-2 ripple active" aria-current="true">
+            <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>News</span>
           </a>
           <a href="NewPost.php" class="list-group-item list-group-item-action py-2 ripple">
             <i class="fas fa-chart-area fa-fw me-3"></i><span>New Post </span>
