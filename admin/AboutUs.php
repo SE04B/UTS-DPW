472031715e2308d229e371997994254198e44daa
Mirfani340 <muhammadirfani340@gmail.com>
2022-07-21 14:08:39 +0700

    Bug still apear in AboutUs control panel, cant upload images

--- admin/AboutUs.php
+++ admin/AboutUs.php
@@ -1,7 +1,10 @@
 <?php require_once('Include/Sessions.php'); ?>
 <?php require_once('Include/Functions.php'); ?>
+<?php require_once('NewFasilitas.php'); ?>
 <?php ConfirmLogin(); ?>
 <?php
+$sqlCheckFasilitas = "SELECT * FROM cms_fasilitas";
+$checkFasilitas = Query($sqlCheckFasilitas);
 	if(isset($_POST['submit'])){
 		$aboutPostContent = mysqli_real_escape_string($con, $_POST['about-post-content']);
 		$visi = mysqli_real_escape_string($con, $_POST['post-visi']);
@@ -13,8 +16,27 @@
 				$_SESSION['successMessage'] = "Post Added Successfully";
 			} else {
 				$_SESSION['errorMessage'] = "Something Went Wrong Please Try Again";
-
 			}
+	}
+
+
+	if(isset($_POST['fasilitas1simpan'])){
+		$name = mysqli_real_escape_string($con, $_POST['fasilitas1']);
+		// $image =
+		if (mysqli_num_rows($checkFasilitas) > 0){
+			$query = "UPDATE cms_fasilitas SET name='$name' WHERE id=1";
+			$exec = Query($query);
+		} else {
+			$query = "INSERT INTO cms_fasilitas (id,name,image) VALUES (1,'$name','')";
+			$exec = Query($query);
+		}
+	} elseif (isset($_POST['fasilitas1remove'])){
+		if (mysqli_num_rows($checkFasilitas) > 0){
+			$query = "DELETE FROM cms_fasilitas WHERE id=1";
+			$exec = Query($query);
+		} else {
+			
+		}
 	}
 ?>
 
@@ -102,17 +124,27 @@
 					<div class="page-title"><h1>Manage About Us</h1></div>
 					<?php echo Message(); ?>
 					<?php echo SuccessMessage(); ?>
+					<?php
+					$sqlCheckVisiMisi = "SELECT *  FROM cms_aboutus";
+					$checkVisiMisi = Query($sqlCheckVisiMisi);
+					if ($checkVisiMisi){
+						$post = mysqli_fetch_assoc($checkVisiMisi);
+						$postAboutus = $post['about'];
+						$postVisi = $post['vision'];
+						$postMisi = $post['mission'];
+					}
+					?>
 					<div>
 						<div class="row">
 							<div class="col-md-12 ">
 								<form method="POST" action="AboutUs.php">
 									<fieldset>
 										<div class="form-group" style="margin-top:1cm ;">
-											<label for="cat_name">Tentang Pondok Pesantren Nurul Jadid</label>
+											<label for="cat_name"><strong>Tentang Pondok Pesantren Nurul Jadid</strong></label>
 											<br>
 											<br>
 											<!-- <input class="form-control input-md" type="text" name="about-post-content" id="about-post-content" placeholder="Ceritakan tentang Pesantren Nurul Jadid"> -->
-											<textarea name="about-post-content" id="about-post-content" cols="100" rows="5" placeholder="Ceritakan tentang Pesantren Nurul Jadid "></textarea>
+											<textarea name="about-post-content" id="about-post-content" cols="100" rows="5" placeholder="Ceritakan tentang Pesantren Nurul Jadid "><?php echo $postAboutus ?></textarea>
 										</div>
 
                                         <div class="form-group" style="margin-top:1cm ;">
@@ -120,7 +152,7 @@
 											<br>
 											<br>
 											<!-- <input class="form-control input-md text-area" type="text" name="post-visi" id="post-visi" placeholder="Tuliskan visi di sini"> -->
-											<textarea name="post-visi" id="post-visi" cols="100" rows="5" placeholder="Tuliskan visi di sini"></textarea>
+											<textarea name="post-visi" id="post-visi" cols="100" rows="5" placeholder="Tuliskan visi di sini"><?php echo $postVisi ?></textarea>
 										</div>
 
                                         <div class="form-group" style="margin-top:1cm ;">
@@ -128,25 +160,54 @@
 											<br>
 											<br>
 											<!-- <input class="form-control input-md" type="text" name="post-misi" id="post-misi" placeholder="Tuliskan misi di sini"> -->
-											<textarea name="post-misi" id="post-misi" cols="100" rows="5" placeholder="Tuliskan misi di sini"></textarea>
+											<textarea name="post-misi" id="post-misi" cols="100" rows="5" placeholder="Tuliskan misi di sini"><?php echo $postMisi ?></textarea>
 										</div>
 
                                         <h3 style="margin-top:1cm;">Fasilitas</h3>
-                                        <button type="button" class="btn btn-success">+ Tambah</button>
-                                        <hr>
 
-                                        <?php ?>
+                                        <?php
+										$sqlShow1 = "SELECT *  FROM cms_fasilitas WHERE id=1";
+										$query = Query($sqlShow1);
+										if (mysqli_num_rows($query) > 0){
+											$post = mysqli_fetch_assoc($query);
+											$postFasilitas1 = $post['name'];
+										} else {
+											$postFasilitas1 = "";
+										}
+										?>
                                         <div class="form-group">
                                             <label for="cat_name">Nama Fasilitas 1</label>
-											<input class="form-control input-md" type="text" name="cat_name" placeholder="Tuliskan nama fasilitas di sini">
-                                            <input type="File" name="post-image" class="form-control">
-                                            <button type="button" class="btn btn-danger" style="margin-top: 0.25cm;">Hapus</button>
+											<input class="form-control input-md" type="text" id="fasilitas1" name="fasilitas1" placeholder="Tuliskan nama fasilitas di sini" value="<?php echo $postFasilitas1 ?>">
+                                            <input type="File" name="fasilitasImage1" id="fasilitasImage1" class="form-control">
+                                            <button type="fasilitas1remove" class="btn btn-danger" name="fasilitas1remove" id="fasilitas1remove" style="margin-top: 0.25cm;">Hapus</button>
+											<button type="fasilitas1simpan" class="btn btn-success" id="fasilitas1simpan" name="fasilitas1simpan" style="margin-top: 0.25cm;">Simpan</button>
                                         </div>
 
                                         <div style="margin-top:0.5cm;"></div>
 
                                         <div class="form-group">
                                             <label for="cat_name">Nama Fasilitas 2</label>
+											<input class="form-control input-md" type="text" name="cat_name" placeholder="Tuliskan nama fasilitas di sini">
+                                            <input type="File" name="post-image" class="form-control">
+                                            <button type="button" class="btn btn-danger" style="margin-top: 0.25cm;">Hapus</button>
+                                        </div>
+
+										<div class="form-group">
+                                            <label for="cat_name">Nama Fasilitas 3</label>
+											<input class="form-control input-md" type="text" name="cat_name" placeholder="Tuliskan nama fasilitas di sini">
+                                            <input type="File" name="post-image" class="form-control">
+                                            <button type="button" class="btn btn-danger" style="margin-top: 0.25cm;">Hapus</button>
+                                        </div>
+
+										<div class="form-group">
+                                            <label for="cat_name">Nama Fasilitas 4</label>
+											<input class="form-control input-md" type="text" name="cat_name" placeholder="Tuliskan nama fasilitas di sini">
+                                            <input type="File" name="post-image" class="form-control">
+                                            <button type="button" class="btn btn-danger" style="margin-top: 0.25cm;">Hapus</button>
+                                        </div>
+
+										<div class="form-group">
+                                            <label for="cat_name">Nama Fasilitas 5</label>
 											<input class="form-control input-md" type="text" name="cat_name" placeholder="Tuliskan nama fasilitas di sini">
                                             <input type="File" name="post-image" class="form-control">
                                             <button type="button" class="btn btn-danger" style="margin-top: 0.25cm;">Hapus</button>
